17ba9a7e49b17bdfc432273cff224d2379401f45
Ikar-code <lucas.lbruno2007@gmail.com>
2026-06-11 14:04:41 +0400

    Add article writing functionality to test_step1.js

--- test_step1.js
+++ test_step1.js
@@ -1,5 +1,6 @@
 import { findTopics } from './src/step1_findTopics.js';
 import { filterNewTopics } from './src/step2_checkDuplicates.js';
+import { writeArticle } from './src/step3_writeArticle.js';
 
 (async () => {
   try {
@@ -10,6 +11,14 @@ import { filterNewTopics } from './src/step2_checkDuplicates.js';
     const newTopics = await filterNewTopics(topics);
     console.log('--- NOUVEAUX SUJETS (insérés dans Supabase) ---');
     console.log(JSON.stringify(newTopics, null, 2));
+
+    if (newTopics.length > 0) {
+      const article = await writeArticle(newTopics[0]);
+      console.log('--- ARTICLE GÉNÉRÉ ---');
+      console.log(JSON.stringify(article, null, 2));
+    } else {
+      console.log('Aucun nouveau sujet à traiter pour cette étape.');
+    }
   } catch (e) {
     console.error('Échec:', e.message);
   }
