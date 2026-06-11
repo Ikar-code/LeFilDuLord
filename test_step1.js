bd64fbbc73db388cae75895774f367115b6f6500
Ikar-code <lucas.lbruno2007@gmail.com>
2026-06-11 14:01:04 +0400

    Update log messages and add newTopics filtering

--- test_step1.js
+++ test_step1.js
@@ -1,10 +1,15 @@
 import { findTopics } from './src/step1_findTopics.js';
+import { filterNewTopics } from './src/step2_checkDuplicates.js';
 
 (async () => {
   try {
     const topics = await findTopics();
-    console.log('--- SUJETS TROUVÉS ---');
+    console.log('--- SUJETS PROPOSÉS ---');
     console.log(JSON.stringify(topics, null, 2));
+
+    const newTopics = await filterNewTopics(topics);
+    console.log('--- NOUVEAUX SUJETS (insérés dans Supabase) ---');
+    console.log(JSON.stringify(newTopics, null, 2));
   } catch (e) {
     console.error('Échec:', e.message);
   }
